5f192129a7a0218ede598be1fdb06521d8be0a3f
Emilia <emilia.assarsson@hotmail.com>
2021-06-24 20:09:34 -0600

    Uppdate1

---
+++ script.js
@@ -0,0 +1,11 @@
+function loadImage ()  {
+    let img = document.createElement('img');
+    img.src = document.querySelector('#favoritePicture').Value;
+    img.classList.add('normalSizedImage');
+
+    let secondbutton = 
+
+}
+
+
+
