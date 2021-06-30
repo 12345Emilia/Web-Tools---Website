0d94bb43d9a2688c3b58ad1c7a21778eb7876e7d
Emilia <emilia.assarsson@hotmail.com>
2021-06-29 20:00:10 -0600

    uppdated function

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function loadImage ()  {
+function loadImage() {
+    
+    // create html element
     let img = document.createElement('img');
-    img.src = document.querySelector('#favoritePicture').Value;
+    // retreieve user url
+    img.src = document.querySelector('#favoritePicture').value;
+    // put image on page
     img.classList.add('normalSizedImage');
+    // document.getElementById('userImage').appendChild(img);
+    
+    let secondButton = document.getElementById('loadImageButton');
+    // secondButton.parentNode.insertBefore(img, secondButton.nextSibling);
+    // let secondbutton = document.getElementById 
 
-    let secondbutton = 
+    secondButton.after(img);
 
 }
 
